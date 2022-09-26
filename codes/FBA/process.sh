48a17e4442961028c2dd02ebf434773e3111906d
lehai-ml <l.haimcl@gmail.com>
2022-09-26 17:03:23 +0100

    Update process.sh
    added calculation of FD FC and FDC

--- codes/FBA/process.sh
+++ codes/FBA/process.sh
@@ -29,10 +29,17 @@ warped_wm_fod_average=warped_wm_fod_average.mif
 native2average_warp=native2average_warp.mif
 average2native_warp=average2native_warp.mif
 
-fixel_mask=fixel_mask/
+fixel_mask=fixel_mask
 
 fod_in_template_space_NOT_REORIENTED=fod_in_template_space_NOT_REORIENTED.mif
+fixel_in_template_space_NOT_REORIENTED=fixel_in_template_space_NOT_REORIENTED
+fd=fd.mif
+fixel_in_template_space=fixel_in_template_space
 
+all_subj_fd=all_subj_fd 
+all_subj_fc=all_subj_fc
+all_subj_log_fc=all_subj_log_fc
+all_subj_fdc=all_subj_fdc
 set -e
 . support_functions.sh
 
@@ -104,21 +111,20 @@ done
 #creating average image of 40 week masks and FODs
 cd $output_folder
 echo '###################################'
-echo '	' Generating average images
-echo '###################################'
-
+echo ''
+echo ''
 run 'generating an average image of 40 weeks masks' \
   mrmath $(IN */*/$warped_mask_in_dHCP_40wk) min -datatype bit - \| mrgrid - regrid -voxel 1.3 -interp nearest OUT:$warped_mask_average
 
 run 'generating an average image of 40 weeks FODs' \
   mrmath $(IN */*/$warped_wm_fod_in_dHCP_40wk) mean - \| mrgrid - regrid -voxel 1.3 -interp sinc OUT:$warped_wm_fod_average
 
+run 'calculating fixel from average mask' \
+  fod2fixel -mask IN:$warped_mask_average -fmls_peak_value 0.06 IN:$warped_wm_fod_average OUT:$fixel_mask
 
-
-
-
-
-
+echo ''
+echo ''
+echo '###################################'
 
 #Registering FODs to average templates
 cd $src
@@ -134,41 +140,34 @@ cd $output_folder/$ID
 run 'registering normalised FODs to average images' \
   mrregister IN:$wm_norm_fod -mask1 IN:$mask IN:$output_folder/$warped_wm_fod_average -nl_warp OUT:$native2average_warp OUT:$average2native_warp
 
-)||continue
-done
+run 'transforming FOD images to average template' \
+  mrtransform IN:$wm_norm_fod -warp IN:$native2average_warp -reorient_fod no OUT:$fod_in_template_space_NOT_REORIENTED
 
+run 'segmenting FOD images to estimate fixels and their fiber density' \
+ fod2fixel -mask IN:$output_folder/$warped_mask_average IN:$fod_in_template_space_NOT_REORIENTED OUT:$fixel_in_template_space_NOT_REORIENTED -afd OUT:$fd
 
+run 'reorienting direction of fixels' \
+ fixelreorient IN:$fixel_in_template_space_NOT_REORIENTED IN:$native2average_warp OUT:$fixel_in_template_space
 
-
-
-
-
-#Fixel analysis
 cd $output_folder
-echo '###################################'
-echo  Calculating fixel from average mask
-echo '###################################'
+id_ses=$(echo $ID | sed 's/\//_/')
+run 'running fixel correspondence and computing fiber density'\
+ fixelcorrespondence IN:$ID/$fixel_in_template_space/$fd IN:$fixel_mask OUT:$all_subj_fd OUT:${id_ses}.mif
 
-run 'calculating fixel from average mask' \
-  fod2fixel -mask IN:$warped_mask_average -fmls_peak_value 0.06 IN:$warped_wm_fod_average OUT:$fixel_mask
+run 'computing fibre cross-section' \
+ warp2metric IN:$ID/$native2average_warp -fc IN:$fixel_mask OUT:$all_subj_fc OUT:${id_ses}.mif
 
+mkdir -p $all_subj_log_fc
+run 'computing log of fibre cross-section' \
+ mrcalc IN:$all_subj_fc/${id_ses}.mif -log OUT:$all_subj_log_fc/${id_ses}.mif
 
+mkdir -p $all_subj_fdc
+run 'computing combined measure FDC' \
+ mrcalc IN:$all_subj_fd/${id_ses}.mif IN:$all_subj_fc/${id_ses}.mif -mult OUT:$all_subj_fdc/${id_ses}.mif
 
-
-
-
-
-cd $src
-
-for ID in ${ID_list[@]}; do
-(
-echo '###################################'
-echo '    '$ID
-echo '###################################'
-
-cd $output_folder/$ID
-#transforming FOD images into template space
-#segmenting FOD images to estimate fixels and their FD
 )||continue
 done
 
+# perform whole brain tracktography on the FOD template
+# reduce biases in tractogram densities
+# smooth the data
