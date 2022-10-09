bd9e44aeba22faae7b42bf0b3510ea53680458d7
lehai-ml <l.haimcl@gmail.com>
2022-10-09 19:16:10 +0100

    Update generate_ID_list.sh

--- codes/FBA/generate_ID_list.sh
+++ codes/FBA/generate_ID_list.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-dwi_data="data"
-rf_data="dhcp_neo_dMRI_derived"
-participants_info="dHCP_participant_info.csv"
-usable_subjects="usable_subj.txt"
-
-all_available_IDs=all_available_IDs.txt
-usable_subj=usable_subj.txt
-
 run 'generate all available ID list' \
   python generate_ID_list.py generate --folder IN:$rf_data --pattern sub*/ses* \| python generate_ID_list.py generate --folder IN:$dwi_data --pattern sub*/ses* \| python generate_ID_list.py generate --file IN:$participants_info --columns 0 2 --prefix sub- ses- --apcolumns 5 3 4 --out OUT:$all_available_IDs
 
@@ -15,7 +7,8 @@ run 'selecting usable ID' \
   python generate_ID_list.py select --file IN:$all_available_IDs --criteria 3 37 '\>=' 2 37 '\<' --group preterm \|  python generate_ID_list.py select --file IN:$all_available_IDs --criteria 3 37 '\>=' 2 37 '\>=' --group term --out OUT:$usable_subjects
 
 if [ ! -f subjects_list.txt ]; then
-    shuf -n 2 $usable_subjects > subjects_list.txt
+    cat $usable_subjects | grep ,term | shuf -n 128  > subjects_list.txt
+    cat $usable_subjects | grep ,preterm >> subjects_list.txt
 fi
 
 
