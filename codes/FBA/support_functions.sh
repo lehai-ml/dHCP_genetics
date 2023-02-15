62e0227ef1f6d4a6cdaaac028d381bdc42322b95
hai <l.haimcl@gmail.com>
2023-02-15 19:16:30 +0000

    added generating wm tract function

--- codes/FBA/support_functions.sh
+++ codes/FBA/support_functions.sh
@@ -205,6 +205,165 @@ function generate_binary_mask {
     eval ${to_eval[@]}
 }
 
+#Convenience function used in generate_wm_tract.
+#usage: generate_binary_ROI_mask ROIs_template ROI1+ROI2+ROI3
+# combine into a single image using mrcalc.
+#e.g. max(max(max(max(binary = 1;binary=2);binary=3)))
+
+function generate_binary_ROI_mask {
+    ROIs_template=$1
+    #IFS='+' read -r -a ROIs <<< $2
+    ROIs=($(echo $2 | tr '+' ' '))
+    output=$3
+    to_eval="mrcalc "
+    for (( i=0; i<${#ROIs[@]}; ++i ));do
+	region="${ROIs[i]}"
+	to_eval+=$(echo $ROIs_template $region -eq " ")
+	if [ $i -gt 0 ];then
+	    to_eval+="-max "
+	fi
+    done
+    to_eval+=$output
+    eval "${to_eval[@]}"
+}
+
+#Convenience function used to either generate (tckgen) or edit (tckedit) tracts
+#usage: generate_wm_tract TEXTfile IDENTIFIER
+#flags:
+# -ROI = parcellation file (e.g., KANA)
+# -tckdo = {tckedit; tckgen}
+# -tckfile = tckfile used to edit with tckedit
+# -fod = FOD file used generate track with tckgen
+# -act = 5TT file used with tckgen
+# -out = output tck file
+# -select = select option with tckgen
+# -seeds = seeds option with tckgen
+# -maxlength = maximum length to select
+# -minlength = minimum length to select
+# -fo = -force overwrite existing file
+# -keep = keep generated temporary files (e.g., seed image)
+# TEXTfile
+# The function is used to parse each line in the text file.
+# Such that each line beginning with an IDENTIFIER is recognised.
+# E.g.,
+# CING_D_R-seed-image: 1+2+3 -> means that a binary seed image will be created with ROIs 1,2,3
+# CING_D_R-seed-sphere: 1,1,1,10 -> means the tckgen will use seed-sphere option instead.
+# NOTE if seed-image is used, seed-sphere must be commented out.
+# CING_D_R-include: 1+2+3 -> means that a binary include image will be created
+# CING_D_R-exclude: 1,1,1,10 -> means a sphere is excluded
+# All the generated binary files is kept as IDENTIFIER-tmp-*.mif
+
+function generate_wm_tract {
+	file=$1
+	identifier=$2
+	include=()
+	while read line; do
+	    if [[ $line == $(echo "$identifier*") ]];then
+	        include+=(${line#$identifier})
+	    fi
+	done < $file
+	cmd=($@)
+	for i in "${!cmd[@]}";do
+	    if [[ ${cmd[$i]} == "-ROI" ]]; then
+		ROI="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-tckdo" ]]; then
+		track_cmd="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-tckfile" ]]; then
+		track="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-fod" ]]; then
+		fod="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-act" ]]; then
+		act="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-out" ]]; then
+		output="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-select" ]]; then
+		select="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-seeds" ]]; then
+		seeds="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-mask" ]]; then
+	        mask_tract="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-maxlength" ]]; then
+	        maxlength="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-minlength" ]]; then
+	        minlength="${cmd[(($i+1))]}"
+	    elif [[ ${cmd[$i]} == "-fo" ]]; then
+		fo="${cmd[$i]}"
+	    elif [[ ${cmd[$i]} == "-keep" ]]; then
+		keep=1
+	    fi
+	done
+	
+	to_eval=()
+	to_eval+=$track_cmd
+	if [[ $track_cmd == "tckgen" ]]; then
+	    if [ -z "${fod+x}" ];then
+		echo "FOD image is required to do tckgen iFOD2"
+		return 0
+	    fi
+	    to_eval+=" $fod"
+	    for n in "${!include[@]}"; do
+		if [[ ${include[$n]} == "-seed_image:" ]]; then
+		    seed_roi=${include[(($n+1))]}
+		    echo -e "${RED}>>> generating seed image ${NC}"
+		    seed_image="$identifier-tmp-seed_image.mif"
+		    (generate_binary_ROI_mask $ROI $seed_roi $seed_image)
+		    to_eval+=" -seed_image $seed_image"
+		elif [[ ${include[$n]} == "-seed_sphere:" ]]; then
+		    seed_sphere=${include[(($n+1))]}
+		    to_eval+=" -seed_sphere $seed_sphere"
+		fi
+	    done
+	    if [ ! -z "${act+x}" ];then
+	        to_eval+=" -act $act"
+	    fi
+	    if [ ! -z "${seeds+x}" ];then
+	        to_eval+=" -seeds $seeds"
+	    fi
+	    if [ ! -z "${select+x}" ];then
+	        to_eval+=" -select $select"
+	    fi
+	elif [[ $track_cmd == "tckedit" ]]; then
+	    if [ -z "${track+x}" ]; then
+	        echo -e "${RED} tck file is needed to do tckedit ${NC}"
+		return 0
+	    fi
+	    to_eval+=" $track"
+	fi
+	for n in "${!include[@]}";do
+	    if [[ ${include[$n]} == "-include:" ]] || [[ ${include[$n]} == "-exclude:" ]] ; then
+	        to_do=${include[$n]%:}
+	        to_include=${include[(($n+1))]}
+	        if [[ $to_include == *"+"* ]] || [ "$to_include" -eq "$to_include" ] ; then
+	            if [ -z "${ROI+x}" ]; then
+	                echo "ROI needed to create include/exclude mask"
+			return 0
+	            fi
+		    region="$identifier-tmp$to_do-$n.mif"
+		    echo 
+	            (generate_binary_ROI_mask $ROI $to_include $region)
+		    to_eval+=" $to_do $region"
+		else
+		    to_eval+=" $to_do $to_include"
+	        fi
+	    fi
+	done
+	if [ ! -z "${mask_tract+x}" ];then
+	    to_eval+=" -mask $mask_tract"
+	fi
+	if [ ! -z "${maxlength+x}" ];then
+	    to_eval+=" -maxlength $maxlength"
+	fi
+	if [ ! -z "${minlength+x}" ];then
+	    to_eval+=" -minlength $minlength"
+	fi
+	to_eval+=" $output $fo"
+	echo "${to_eval[@]}"
+	eval "${to_eval[@]}"
+	if [ -z "${keep+x}" ];then
+	    rm $identifier-tmp-*.mif 2>/dev/null
+	fi
+}
+
 #convenient function that copy header into a new file
 function copy_header {
 	file1=$1
