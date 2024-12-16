6d86adcff2e13baaf8b29730daed1a8a66d98719
lehai-ml <l.haimcl@gmail.com>
2024-12-16 00:42:05 +0000

    removed docs

--- codes/docs/analysis/MLtraining/__init__.py
+++
@@ -1,6 +0,0 @@
-from .ML_utils import splitting_dataset_into_k_fold, get_model_bias
-from .ML_utils import print_scores
-from .ML_utils import remove_correlated_features, merge_features_names, merge_features_values, High_Corr_Remover
-from .ML_utils import Retain_non_zero_features
-from .ML_utils import Select_Features_Univariate
-from .ML_utils import myPipe, MyLabelBinarizer
